ffce99e8905621c305076c439e62b4e6b131f287
Richard Schwab <richard.schwab@valtech.com>
2020-10-11 13:54:25 +0200

    remove js consts that were obsoleted by the previous commit

--- static/modules/const.js
+++ static/modules/const.js
@@ -2,10 +2,6 @@
 
 export const name = "const";
 
-// This value defines the duration the conditions to swap the displayed tables have to apply before it will be acted on.
-// This value is only checked against every 100ms.
-export const RCBOARD_CHECK_AND_HIDE_CHECK_INTERVAL = 2500;
-
 // Duration for the tables to be swapped.
 export const RCBOARD_CHECK_AND_HIDE_DISPLAY_DURATION = 30000;
 
